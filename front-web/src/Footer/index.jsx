c904c53db4e450584231a3a38c2680bc471a4839
Ruan-_pb <ruancruzsoares@hotmail.com>
2021-01-12 15:04:11 -0300

    referenciando meus dados ao icone Instagram, Linkedim e youtube

--- front-web/src/Footer/index.jsx
+++ front-web/src/Footer/index.jsx
@@ -7,19 +7,22 @@ function Footer(){
 
         <footer className="main-footer">
 
-            App desenvolvido durante a 2º edição do evento "Semana DevSuperior"
+            App desenvolvido durante a 2º edição do evento "Semana DevSuperio"<br/>
+        
+            
+            
 
             
             <div className="footer-icons">
-                <a href="https://www.youtube.com/" target="_new">
+                <a href="https://www.youtube.com/channel/UCIKwDZPlSfK6FxWyWXNGXDg" target="_new">
                     <Youtube/>
                 </a>
 
-                <a href="https://www.linkedin.com/feed/" target="_new">
+                <a href="https://www.linkedin.com/in/ruan-cruz-909365197/" target="_new">
                     <Linkedin/>
                 </a>
 
-                <a href="https://www.instagram.com/" target="_new">
+                <a href="https://www.instagram.com/ruan.soares_/" target="_new">
                     <Instagram/>
                 </a>
             </div>
